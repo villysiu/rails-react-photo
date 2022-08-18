86394bcbe7c54bab322d1069c4f58ea4797ed401
villysiu <villysiu@gmail.com>
2022-08-17 19:15:47 -0700

    dropdown menu

--- photo/config/routes.rb
+++ photo/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'welcome/home'
   get '/app', to: 'welcome#app', as: 'app'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#app'
+  root 'welcome#home'
 end
